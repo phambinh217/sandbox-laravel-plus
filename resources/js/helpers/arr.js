05999006ea64411452fd3cc3d19542380bb81df1
phambinh217 <phambinh217@gmail.com>
2020-10-30 16:10:53 +0700

    xoa dau cham phay

--- resources/js/helpers/arr.js
+++ resources/js/helpers/arr.js
@@ -1,13 +1,13 @@
 export function arrRandom (arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)]
 }
 
 export function arrChunk (array, chunkSize) {
     return [].concat.apply([], array.map(function (elem, i) {
-        return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];
-    }));
+        return i % chunkSize ? [] : [array.slice(i, i + chunkSize)]
+    }))
 }
 
 export function arrFirst (arr) {
-    return arr.find(true);
+    return arr.find(true)
 }
